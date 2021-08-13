28e1fe02484def87e15c0e9121f22b81db8d83aa
amazingefren <efrentheroker@hotmail.com>
2021-08-13 14:12:16 -0600

    post modeling wip

--- src/post/post.model.ts
+++ src/post/post.model.ts
@@ -1,7 +1,52 @@
-import { ObjectType } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
+import { User } from '../user/user.model'
 import 'reflect-metadata';
 
 @ObjectType()
 export class Post {
+  @Field(() => Number)
+  id: number;
 
+  @Field(()=>User)
+  author: User;
+
+  @Field(()=>Number)
+  authorId: number
+
+  @Field(() => String)
+  content: string;
+
+  @Field(() => Date)
+  date: Date;
+
+  @Field(() => Date)
+  updated: Date;
+
+  @Field(() => [PostHistory])
+  history: PostHistory[];
+
+  @Field(()=>[User])
+  likes: User[];
+
+  @Field(()=>[Number])
+  likedIds: number[];
+
+  @Field(()=>[Post])
+  replies: Post[];
+}
+
+@ObjectType()
+export class PostHistory {
+  @Field(() => Number)
+  id: number;
+
+  @Field(() => String)
+  content: string;
+
+  /* 1 parent, or multiple: can be swapped later in relation to post.schema.ts
+  * parent: Post + parentId: Post.id (aka number)?
+  * or: parent => number || parent => post || parentId => number
+  */
+  @Field(() => Post)
+  parent: Post;
 }
