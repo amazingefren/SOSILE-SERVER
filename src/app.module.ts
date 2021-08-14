adb3ac0081f8df430b94adca2d165f71387ae32d
amazingefren <efrentheroker@hotmail.com>
2021-08-14 13:43:33 -0600

    auth module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,15 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { AuthModule } from './auth/auth.module';
 
 import configuration from './config/configuration';
 
 @Module({
   imports: [
     UserModule,
+    PostModule,
+    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
@@ -21,7 +24,6 @@ import configuration from './config/configuration';
       debug: true,
       playground: false
     }),
-    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService],
