cccb0c7658e78aa8b601e3f6dab3e49a6bc36c32
amazingefren <efrentheroker@hotmail.com>
2021-08-15 22:15:13 -0600

    nightly: empty inits

---
+++ src/auth/interceptors/jwt.interceptor.ts
@@ -0,0 +1,5 @@
+/*
+NOTE: Interceptor AOP as Middlware on Request and Response taps.
+this might be what I want to use to provide context for the Authentication 
+and Authorization guards
+*/
--- src/prisma/prisma.filter.ts
+++ src/prisma/prisma.filter.ts
@@ -1,1 +1,29 @@
-//TODO:
+/* import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+} from '@nestjs/common';
+import { FastifyRequest, FastifyReply } from 'fastify';
+import { Prisma } from '@prisma/client'
+
+@Catch(
+  Prisma.PrismaClientKnownRequestError,
+
+)
+export class AuthExceptionFilter implements ExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const req = ctx.getRequest<FastifyRequest>();
+    const rep = ctx.getResponse<FastifyReply>();
+    const response = exception.message
+
+    
+
+
+    // console.log(req)
+    console.log(rep)
+    console.log(ctx)
+
+    // console.log(status)
+  }
+} */
